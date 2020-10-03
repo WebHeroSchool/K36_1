52e03a0a34795bf74d9681b09ad171eaf96405dc
Anastassia Varabei <nastenkaok1984@gmail.com>
2020-10-03 21:09:33 +0300

    added files js

---
+++ app.js
@@ -0,0 +1,92 @@
+ShowChannel('work', '#channel for work');
+
+const channelButtons = document.querySelectorAll('.wrap__button'),
+      messageBox = document.querySelector('textarea[name="massage"]');
+
+channelButtons.forEach(function(elem) {
+
+  elem.addEventListener("click", function(event) {
+
+    let channelId = event.target.dataset.channelId,
+        channelName = event.target.innerHTML;
+
+    ShowChannel(channelId, channelName);
+
+  });
+
+})
+
+messageBox.addEventListener("keyup", function(event) {
+
+  if (event.code === "Enter") {
+
+    AddMessage({
+
+      authorId: 188372,
+      authorName: "John",
+      time: Math.floor(Date.now() / 1000),
+      message: messageBox.value
+
+    });
+
+    messageBox.value = '';
+    ScrollMessagesToBottom();
+
+  }
+
+});
+
+function AddMessage(messageData) {
+
+  let messagesWrapper = document.querySelector('.wrapper__content'),
+      cloned = document.querySelector('.hidden-data > .content__message').cloneNode(true),
+      img = cloned.querySelector('img'),
+      userName = cloned.querySelector('.information__username'),
+      messageText = cloned.querySelector('.context__text'),
+      messageTime = cloned.querySelector('.information__time');
+
+  img.src = 'image/av/'+messageData.authorId+'.png';
+  userName.innerHTML = messageData.authorName;
+  messageText.innerHTML = messageData.message;
+  messageTime.innerHTML = GetMessageDateTime(messageData.time);
+
+  messagesWrapper.append(cloned);
+
+}
+
+function ShowChannel(channelId, channelName) {
+
+  let channelData = messages.channels[channelId],
+      messagesWrapper = document.querySelector('.wrapper__content'),
+      channelNameEl = document.querySelector('.header__text');
+
+  messagesWrapper.innerHTML = '';
+  channelNameEl.innerHTML = channelName;
+
+  channelData.forEach(function(messageData) {
+
+    AddMessage(messageData);
+
+  });
+
+}
+
+function GetMessageDateTime(timestamp) {
+
+  let date = new Date(timestamp * 1000);
+
+  return AddLeadingZeroes(date.getDate()) + '.' + AddLeadingZeroes(date.getMonth()+1) + '.' + date.getFullYear() + ' ' + AddLeadingZeroes(date.getHours()) + ':' + AddLeadingZeroes(date.getMinutes());
+
+}
+
+function AddLeadingZeroes(n) {
+
+  return String("00" + n).slice(-2);
+
+}
+
+function ScrollMessagesToBottom() {
+
+  window.scrollTo(0,document.body.scrollHeight);
+
+}
---
+++ messages.data.js
@@ -0,0 +1,129 @@
+let messages = {
+
+  channels: {
+
+    work: [
+
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 298473,
+        authorName: "Mary",
+        time: 1592005800,
+        message: "O-ho-ho!"
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      }
+
+    ],
+
+    flood: [
+
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      },
+      {
+
+        authorId: 188372,
+        authorName: "John",
+        time: 1592005800,
+        message: "Working from a van in Australia isn’t feasible if you need internet. It may have improved over the last years but I spent some time in a camper van around Tasmania and internet was a real problem (and Tasmania is tiny compared to the rest of the country)."
+
+      }
+
+    ],
+
+  }
+
+};
